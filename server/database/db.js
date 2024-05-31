c0deee7d88b5e21c1dc06c846450aa59b8e0bcf0
Shalemraja <82069264+Shalemraja777@users.noreply.github.com>
2024-05-31 15:03:46 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@ac-tz54972-shard-00-00.ta9g9bn.mongodb.net:27017,ac-tz54972-shard-00-01.ta9g9bn.mongodb.net:27017,ac-tz54972-shard-00-02.ta9g9bn.mongodb.net:27017/?ssl=true&replicaSet=atlas-4jt23a-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL = process.env.MONGO_URL;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
